e18594a64eb0048b57cd7af2b2e788f821b7198a
AyoRichie1-ops <onanusiabdullateef@gmail.com>
2023-12-23 22:43:38 +0100

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-success-subtle text-success text-center py-3 '>
         <h1 className='fw-light'>&copy;{new Date().getFullYear()} All Rights Reserved</h1>
-        <h4 className='mt-1 fw-bold'>The Meal Place</h4>
+        <h4 className='mt-1 fw-bold'>🍔The Meal Place</h4>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='hero-wrapper px-4 d-flex justify-content-center justify-content-md-start align-items-center'>
       <div className="text-start">
-        <p className="display-5 text-success fs-18">
+        <p className="display-5 text-success fs-18 fw-normal">
           Healthy Foods
         </p>
         <h1 className="text-white fw-light">
